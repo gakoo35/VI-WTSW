93852f23f78b7030f01c6c454f232c25ee816d44
Gael Koch <gael.koch@edu.hefr.ch>
2022-10-21 16:08:15 +0200

    ajout de la map du monde.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from dash import Dash, dcc, html, Input, Output
 import plotly.graph_objects as go
+import plotly.express as px
+import pandas as pd
 
 app = Dash(__name__)
 
@@ -14,6 +16,7 @@ app.layout = html.Div([
         clearable=False,
     ),
     dcc.Graph(id="graph"),
+    dcc.Graph(id="map")
 ])
 
 
@@ -25,6 +28,30 @@ def display_color(color):
         data=go.Bar(y=[2, 3, 1], # replace with your own data source
                     marker_color=color))
     return fig
+
+@app.callback(
+    Output("map", "figure"), 
+    Input("dropdown", "value"))
+def display_map(dataset):
+    df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv')
+    df = df.drop('GDP (BILLIONS)', axis=1)
+    df['livable'] = df['COUNTRY'].isin(['Switzerland'])
+    df["livable"] = df["livable"].astype(int)
+    df["livable"] = df["livable"].astype(str)
+
+    fig = go.Figure(data=go.Choropleth(
+        locations = df['CODE'],
+        z = df['livable'],
+        text = df['COUNTRY'],
+        colorscale = 'Greens',
+        marker_line_color='darkgray',
+        marker_line_width=0.5,
+    ))
+    fig.update_geos(
+        showocean=True, oceancolor="LightBlue",
+    )
+    return fig
+
 
 
 app.run_server(debug=True)
