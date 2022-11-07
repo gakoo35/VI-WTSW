60ce5ee0a663bce3f8186d27be413bc537fda649
Gael Koch <gael.koch@edu.hefr.ch>
2022-11-07 23:41:17 +0100

    ajout du graphique em bar. Encore besoin de changer les couleurs et le texte affiché

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import plotly.graph_objects as go
-from dash import Dash, dcc, html, Input, Output
+from dash import Dash, dcc, html, Input, Output, State
+import plotly.express as px
 
 app = Dash(__name__)
 
@@ -22,17 +23,13 @@ app.layout = html.Div([
     dcc.Markdown('''
         ## 🌏 Where you can go live
         '''),
-    dcc.Dropdown(
-        id="dropdown",
-        options=['Gold', 'MediumTurquoise', 'LightGreen'],
-        value='Gold',
-        clearable=False,
-    ),
-    dcc.Graph(id="graph"),
-    dcc.Graph(id="map")
+    dcc.Graph(id="barchart"),
+    dcc.Graph(id="map"),
+    dcc.Slider(0, 10, 1,
+               value=0,
+               id='my-slider'
+    ), # https://community.plotly.com/t/slider-with-play-button-for-animations-independent-of-plotly/53188/11 --> voir pour play button mais marche pas 
 ], className='markdown-body')
-
-
 
 # world map ------------------------------------------------------------------------------
 
@@ -64,101 +61,55 @@ for i in range(10):
     df_temp = pd.concat([df_color, df_temp], ignore_index=True)
     data_slider.append(df_temp)
 
-def frame_args(duration):
-    return {
-            "frame": {"duration": duration},
-            "mode": "immediate",
-            "fromcurrent": True,
-            "transition": {"duration": duration, "easing": "linear"},
-        }
-
-# still need to remove the infobar on the right and the trace 0 on the hovertext
 @app.callback(
     Output("map", "figure"), 
-    Input("dropdown", "value"))
-def display_map(dataset):
-    frames = []
-    for i in range(0, len(data_slider)):
-        frame = go.Frame(data=[go.Choropleth(
-            locations = data_slider[i]['CODE'],
-            z = data_slider[i]['livable'],
-            colorscale = 'Greens',
-            marker_line_color='darkgray',
-            marker_line_width=0.5,
-            hovertemplate =
-                "<b>"+data_slider[i]['COUNTRY']+" </b><br>" +
-                "longitude: "+str(1)+"<br>" + # change values here if needed --> else remove
-                "latitude: "+str(2)+"<br>" , # change values here if needed --> else remove
-            )],
-            name=str(2022+i))
-        frames.append(frame)
-
-
+    Input("my-slider", "value"))
+def display_map(year):
     fig = go.Figure(
         data=[go.Choropleth(
-            locations = data_slider[0]['CODE'],
-            z = data_slider[0]['livable'],
+            locations = data_slider[year]['CODE'],
+            z = data_slider[year]['livable'],
             colorscale = 'Greens',
             marker_line_color='darkgray',
             marker_line_width=0.5,
             hovertemplate =
-                "<b>"+data_slider[0]['COUNTRY']+" </b><br>" +
+                "<b>"+data_slider[year]['COUNTRY']+" </b><br>" +
                 "longitude: "+str(1)+"<br>" + # change values here if needed --> else remove
                 "latitude: "+str(2)+"<br>" , # change values here if needed --> else remove
-        )],
-        frames=frames
+        )]
     )
     fig.update_geos(
         showocean=True, oceancolor="LightBlue",
     )
-
-    sliders = [{"pad": {"b": 10, "t": 60},
-                "len": 0.9,
-                "x": 0.1,
-                "y": 0,
-                "steps": [ {
-                        "args": [[f.name], frame_args(0)],
-                        "label": str(2022+k),
-                        "method": "animate",
-                    }
-                    for k, f in enumerate(fig.frames)
-                ],
-            }
-        ]
 
     fig.update_layout(
             title='Map of countries you could live in :',
             #width=1200,
             height=800,
             #autosize=False,
-            scene=dict(
-                    zaxis=dict(range=[-0.1, 6.8], autorange=True),
-                    ),
-            updatemenus = [
-            {
-                "buttons": [
-                    {
-                        "args": [None, frame_args(1000)],
-                        "label": "&#9654;", # play symbol
-                        "method": "animate",
-                    },
-                    {
-                        "args": [[None], frame_args(0)],
-                        "label": "&#9724;", # pause symbol
-                        "method": "animate",
-                    },
-                ],
-                "direction": "left",
-                "pad": {"r": 10, "t": 70},
-                "type": "buttons",
-                "x": 0.1,
-                "y": 0,
-            }
-            ],
-            sliders=sliders
     )
     return fig
 
+
+# ------------------- bar chart -----------------
+barchartDF = pd.DataFrame()
+barchartDF['years'] = [2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030, 2031, 2032]
+barchartDF['investissement'] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+barchartDF['interet'] = [0, 0, 0, 1, 1, 2, 4, 6, 10, 15, 18]
+
+@app.callback(
+    Output("barchart", "figure"), 
+    Input("my-slider", "value"))
+def display_map(year):
+    # still need to figure out the colors and what is displayed when hover --> for the colors --> highlight the selected year --> https://stackoverflow.com/questions/72087364/plotly-change-colors-of-specific-bars
+    fig = px.bar(barchartDF, 
+        x="years", 
+        y=["investissement", "interet"], 
+        title="Coumpound interest"
+        )
+    #fig.update_traces(marker_color='green')
+    print(year)
+    return fig
 
 
 app.run_server(debug=True)
